30483e5a04ae726ffa3868790ba6d31e9104fad2
Luciano Colosio <unlucio@gmail.com>
2014-06-13 23:47:54 +0400

    JADE files support * Preparing Test cases for JADE feature * Some html -> renaming * Some More Renaming

--- test/spaSpec.js
+++ test/spaSpec.js
@@ -11,15 +11,22 @@ var through = require("through2");
 var spa = require("../index.js");
 
 describe("spa", function () {
-    var basicCase = function (name, pipelines) {
+    /**
+     * NB --> `sourceKind` parameter, introduced for convenience, may be _ONLY_ html OR jade
+     *
+     * @param {String} sourceKind
+     * @param {String} name
+     * @param {Object} pipelines
+     */
+    var basicCase = function (sourceKind, name, pipelines) {
         return function () {
-            var expected = gulp.src("./test/expected/" + name + "/**/*")
+            var expected = gulp.src("./test/" + sourceKind + "/expected/" + name + "/**/*")
                 .pipe(exclude(function isNull (file) {
                     return file.isNull();
                 }));
-            var actual = gulp.src("./test/fixtures/" + name + "/*.html")
-                .pipe(spa.html({
-                    assetsDir: "./test/fixtures/" + name,
+            var actual = gulp.src("./test/" + sourceKind + "/fixtures/" + name + "/*." + sourceKind)
+                .pipe(spa[sourceKind]({
+                    assetsDir: "./test/"+ sourceKind +"/fixtures/" + name,
                     pipelines: pipelines
                 }));
             return actual.should.produce.sameFilesAs(expected);
@@ -32,6 +39,10 @@ describe("spa", function () {
         return obj;
     };
 
+
+    /**
+     *  HTML pipe tests
+     */
     describe(".html()", function () {
         var tagTypes = [{
             type: "css",
@@ -53,11 +64,11 @@ describe("spa", function () {
                 };
 
                 it("should produce only one " + tagName + " tag",
-                    basicCase("html-one-" + tagName + "-tag", createKeyValue(type, createConcat())));
+                    basicCase("html", "one-" + tagName + "-tag", createKeyValue(type, createConcat())));
                 it("should preserve leading slash in paths for " + tagName + " tag",
-                    basicCase("html-preserve-leading-slash-one-" + tagName + "-tag", createKeyValue(type, createConcat())));
+                    basicCase("html", "preserve-leading-slash-one-" + tagName + "-tag", createKeyValue(type, createConcat())));
                 it("should not add leading slash to " + tagName + " tags' paths that didn't have it",
-                    basicCase("html-dont-add-leading-slash-one-" + tagName + "-tag", createKeyValue(type, createConcat())));
+                    basicCase("html", "dont-add-leading-slash-one-" + tagName + "-tag", createKeyValue(type, createConcat())));
             });
         });
 
@@ -67,21 +78,21 @@ describe("spa", function () {
                 var tagName = tagType.tagName;
 
                 it("should produce multiple " + tagName + " tags using original whitespace convention",
-                    basicCase("html-many-" + tagName + "-tag", {}));
+                    basicCase("html", "many-" + tagName + "-tag", {}));
                 it("should preserve leading slash in paths for " + tagName + " tags",
-                    basicCase("html-preserve-leading-slash-many-" + tagName + "-tag", {}));
+                    basicCase("html", "preserve-leading-slash-many-" + tagName + "-tag", {}));
                 it("should not add leading slash to " + tagName + " tags' paths that didn't have it",
-                    basicCase("html-dont-add-leading-slash-many-" + tagName + "-tag", {}));
+                    basicCase("html", "dont-add-leading-slash-many-" + tagName + "-tag", {}));
             });
         });
 
-        it("should use the pipeline titled `main` for the main file", basicCase("html-use-pipeline-named-main", {
+        it("should use the pipeline titled `main` for the main file", basicCase("html", "use-pipeline-named-main", {
             main: function (files) {
                 return files.pipe(rename("foo.html"));
             }
         }));
 
-        it("should work with multiple html files", basicCase("html-multiple-builds", {
+        it("should work with multiple html files", basicCase("html", "multiple-builds", {
             js: function (files) {
                 return files.pipe(concat("joined.js"));
             },
@@ -90,11 +101,11 @@ describe("spa", function () {
             }
         }));
 
-        it("should not insert files that aren't CSS or JS", basicCase("html-dont-insert-unknown", {}));
+        it("should not insert files that aren't CSS or JS", basicCase("html", "dont-insert-unknown", {}));
 
-        it("should work with html files without any builds", basicCase("html-no-builds", {}));
+        it("should work with html files without any builds", basicCase("html", "no-builds", {}));
 
-        it("should work when css files are moved to another directory", basicCase("html-link-tags-moved-to-directory", {
+        it("should work when css files are moved to another directory", basicCase("html", "link-tags-moved-to-directory", {
             css: function (files) {
                 return files.pipe(rename({ dirname: "css" }));
             }
@@ -128,19 +139,19 @@ describe("spa", function () {
                 contents: null
             }));
 
-            var expected = gulp.src("./test/expected/html-ignore-null-files/*");
-            var actual = gulp.src("./test/fixtures/html-ignore-null-files/*").pipe(stream);
+            var expected = gulp.src("./test/html/expected/ignore-null-files/*");
+            var actual = gulp.src("./test/html/fixtures/ignore-null-files/*").pipe(stream);
             return actual.should.produce.sameFilesAs(expected);
         });
 
         it("should let the source be defined in the gulpfile if build has no sources", function () {
-            var expected = gulp.src("./test/expected/html-source-defined-in-gulpfile/*");
-            var actual = gulp.src("./test/fixtures/html-source-defined-in-gulpfile/*.html")
+            var expected = gulp.src("./test/html/expected/source-defined-in-gulpfile/*");
+            var actual = gulp.src("./test/html/fixtures/source-defined-in-gulpfile/*.html")
                 .pipe(spa.html({
-                    assetsDir: "./test/fixtures/html-source-defined-in-gulpfile/",
+                    assetsDir: "./test/html/fixtures/source-defined-in-gulpfile/",
                     pipelines: {
                         js: function () {
-                            return gulp.src(["file1.js", "file2.js"], { cwd: "./test/fixtures/html-source-defined-in-gulpfile/" })
+                            return gulp.src(["file1.js", "file2.js"], { cwd: "./test/html/fixtures/source-defined-in-gulpfile/" })
                                 .pipe(concat("joined.js"));
                         }
                     }
@@ -148,16 +159,16 @@ describe("spa", function () {
             return actual.should.produce.sameFilesAs(expected);
         });
 
-        it("should allow overriding options for a build in the declaration", basicCase("expected/html-override-build-options", {}));
+        it("should allow overriding options for a build in the declaration", basicCase("html", "expected/override-build-options", {}));
 
         it("should allow overriding options for a build in the declaration (sources in gulp file)", function () {
-            var expected = gulp.src("./test/expected/html-override-build-options-source-defined-in-gulpfile/*");
-            var actual = gulp.src("./test/fixtures/html-override-build-options-source-defined-in-gulpfile/*.html")
+            var expected = gulp.src("./test/html/expected/override-build-options-source-defined-in-gulpfile/*");
+            var actual = gulp.src("./test/html/fixtures/override-build-options-source-defined-in-gulpfile/*.html")
                 .pipe(spa.html({
-                    assetsDir: "./test/fixtures/html-override-build-options/",
+                    assetsDir: "./test/html/fixtures/override-build-options/",
                     pipelines: {
                         js: function () {
-                            return gulp.src(["file1.js", "file2.js"], { cwd: "./test/fixtures/html-override-build-options/" })
+                            return gulp.src(["file1.js", "file2.js"], { cwd: "./test/html/fixtures/override-build-options/" })
                                 .pipe(concat("joined.js"));
                         }
                     }
@@ -166,7 +177,145 @@ describe("spa", function () {
         });
     });
 
-    it("should allow multiple builds through the same pipeline", basicCase("html-multiple-builds-with-same-pipeline", {
+
+    /**
+     * JADE pipe tests
+     */
+    describe(".jade()", function () {
+        var tagTypes = [{
+            type: "css",
+            tagName: "link"
+        }, {
+
+            type: "js",
+            tagName: "script"
+        }];
+
+        describe("with one output file", function () {
+            tagTypes.forEach(function (tagType) {
+                var type = tagType.type;
+                var tagName = tagType.tagName;
+                var createConcat = function () {
+                    return function (files) {
+                        return files.pipe(concat("joined." + type));
+                    };
+                };
+
+                it("should produce only one " + tagName + " tag",
+                    basicCase("jade", "one-" + tagName + "-tag", createKeyValue(type, createConcat())));
+                it("should preserve leading slash in paths for " + tagName + " tag",
+                    basicCase("jade", "preserve-leading-slash-one-" + tagName + "-tag", createKeyValue(type, createConcat())));
+                it("should not add leading slash to " + tagName + " tags' paths that didn't have it",
+                    basicCase("jade", "dont-add-leading-slash-one-" + tagName + "-tag", createKeyValue(type, createConcat())));
+            });
+        });
+
+        /*describe("with multiple output files", function () {
+            tagTypes.forEach(function (tagType) {
+                var type = tagType.type;
+                var tagName = tagType.tagName;
+
+                it("should produce multiple " + tagName + " tags using original whitespace convention",
+                    basicCase("jade", "many-" + tagName + "-tag", {}));
+                it("should preserve leading slash in paths for " + tagName + " tags",
+                    basicCase("jade", "preserve-leading-slash-many-" + tagName + "-tag", {}));
+                it("should not add leading slash to " + tagName + " tags' paths that didn't have it",
+                    basicCase("jade", "dont-add-leading-slash-many-" + tagName + "-tag", {}));
+            });
+        });
+
+        it("should use the pipeline titled `main` for the main file", basicCase("jade", "use-pipeline-named-main", {
+            main: function (files) {
+                return files.pipe(rename("foo.html"));
+            }
+        }));
+
+        it("should work with multiple html files", basicCase("jade", "multiple-builds", {
+            js: function (files) {
+                return files.pipe(concat("joined.js"));
+            },
+            css: function (files) {
+                return files.pipe(concat("joined.css"));
+            }
+        }));
+
+        it("should not insert files that aren't CSS or JS", basicCase("jade", "dont-insert-unknown", {}));
+
+        it("should work with html files without any builds", basicCase("jade", "no-builds", {}));
+
+        it("should work when css files are moved to another directory", basicCase("jade", "link-tags-moved-to-directory", {
+            css: function (files) {
+                return files.pipe(rename({ dirname: "css" }));
+            }
+        }));
+
+        it("should throw an error if streaming is attempted", function () {
+            var stream = spa.html();
+            void function () {
+                stream.write(new File({
+                    path: "/foo.html",
+                    contents: through.obj()
+                }));
+            }.should.throw("Streaming not supported");
+        });
+
+        it("should throw an error if the file has an `endbuild` instruction without `build` instruction", function () {
+            var stream = spa.html();
+            void function () {
+                stream.write(new File({
+                    path: "/foo.html",
+                    contents: new Buffer("<html><!-- endbuild --></html>")
+                }));
+            }.should.throw("Found an endbuild block without build block");
+        });
+
+        it("should ignore null files", function () {
+            var stream = spa.html();
+
+            stream.write(new File({
+                path: "/meow.html",
+                contents: null
+            }));
+
+            var expected = gulp.src("./test/html/expected/ignore-null-files/*");
+            var actual = gulp.src("./test/html/fixtures/ignore-null-files/*").pipe(stream);
+            return actual.should.produce.sameFilesAs(expected);
+        });
+
+        it("should let the source be defined in the gulpfile if build has no sources", function () {
+            var expected = gulp.src("./test/html/expected/source-defined-in-gulpfile/*");
+            var actual = gulp.src("./test/html/fixtures/source-defined-in-gulpfile/*.html")
+                .pipe(spa.html({
+                    assetsDir: "./test/html/fixtures/source-defined-in-gulpfile/",
+                    pipelines: {
+                        js: function () {
+                            return gulp.src(["file1.js", "file2.js"], { cwd: "./test/html/fixtures/source-defined-in-gulpfile/" })
+                                .pipe(concat("joined.js"));
+                        }
+                    }
+                }));
+            return actual.should.produce.sameFilesAs(expected);
+        });
+
+        it("should allow overriding options for a build in the declaration", basicCase("jade", "expected/override-build-options", {}));
+
+        it("should allow overriding options for a build in the declaration (sources in gulp file)", function () {
+            var expected = gulp.src("./test/html/expected/override-build-options-source-defined-in-gulpfile/*");
+            var actual = gulp.src("./test/html/fixtures/override-build-options-source-defined-in-gulpfile/*.html")
+                .pipe(spa.html({
+                    assetsDir: "./test/html/fixtures/override-build-options/",
+                    pipelines: {
+                        js: function () {
+                            return gulp.src(["file1.js", "file2.js"], { cwd: "./test/html/fixtures/override-build-options/" })
+                                .pipe(concat("joined.js"));
+                        }
+                    }
+                }));
+            return actual.should.produce.sameFilesAs(expected);
+        });*/
+    });
+
+    it("should allow multiple builds through the same pipeline", basicCase("html", "multiple-builds-with-same-pipeline", {
         js: function (files) {
             return files.pipe(rename(function (file) {
                 // do nothing, dummy pipeline
@@ -181,7 +330,7 @@ describe("spa", function () {
     });
 
     it("should allow multiple builds in different files through the same pipeline",
-        basicCase("html-multiple-builds-in-different-files-with-same-pipeline", {
+        basicCase("html", "multiple-builds-in-different-files-with-same-pipeline", {
             js: function (files) {
                 id += 1;
                 return files.pipe(concat("joined" + id + ".js"));
